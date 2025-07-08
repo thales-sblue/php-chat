60b95d7995974fea39e909d02839c185c2259d6a
thales.santos <thales.santos@coopertec.com.br>
2025-07-08 15:05:05 -0300

    configurando client seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Client;
 
 class ClientSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Client::create([
+            'name' => 'João Silva',
+            'cpf_cnpj' => '12345678901',
+            'email' => 'joao@teste.com',
+            'type' => 'pre',
+            'balance' => 20.00
+        ]);
+
+        Client::create([
+            'name' => 'Maria Oliveira',
+            'cpf_cnpj' => '98765432100',
+            'email' => 'maria@teste.com',
+            'type' => 'post',
+            'limit' => 100.00
+        ]);
     }
 }
