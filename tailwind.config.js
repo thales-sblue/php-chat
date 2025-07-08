1918e6d10e60a89833908d53f2e5861453c9af8c
thales.santos <thales.santos@coopertec.com.br>
2025-07-08 17:09:45 -0300

    configurando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './resources/**/*.vue',
+    './resources/**/*.js',
+    './resources/**/*.ts',
+    './resources/**/*.jsx',
+    './resources/**/*.tsx',
+    './resources/views/**/*.blade.php',
+  ],
   theme: {
     extend: {},
   },
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
@@ -10,7 +9,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
         [vue()],
     ],
     resolve: {
